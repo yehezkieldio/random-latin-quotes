8d1dd724e55ff88de9633afef3a3a1b932190389
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2024-08-10 18:09:09 +0800

    initial: initial commit

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::sync::{Mutex, MutexGuard};
+
+use actix_web::{get, web, App, HttpServer, Responder};
+use rand::seq::SliceRandom;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Quote {
+    text: String,
+    author: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Quotes {
+    quotes: Vec<Quote>,
+}
+
+struct AppState {
+    quotes: Mutex<Vec<Quote>>,
+}
+
+#[get("/")]
+async fn random_quote(data: web::Data<AppState>) -> impl Responder {
+    let quotes: MutexGuard<Vec<Quote>> = data.quotes.lock().unwrap();
+    let quote: Option<&Quote> = quotes.choose(&mut rand::thread_rng());
+
+    match quote {
+        Some(quote) => web::Json(Quote {
+            text: quote.text.clone(),
+            author: quote.author.clone(),
+        }),
+        None => web::Json(Quote {
+            text: "No quotes available".to_string(),
+            author: "System".to_string(),
+        }),
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let quotes_file = std::fs::File::open("data/quotes.json").expect("file not found");
+    let quotes: Quotes = serde_json::from_reader(quotes_file).expect("error while reading file");
+
+    let app_state = web::Data::new(AppState {
+        quotes: Mutex::new(quotes.quotes),
+    });
+
+    HttpServer::new(move || App::new().app_data(app_state.clone()).service(random_quote))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
